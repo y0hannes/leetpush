5455b0b369428fabb45d3be05efe0c20a9342851
y0hannes <yohanneset3@gmail.com>
2025-08-02 10:52:03 +0300

    feat: Add GitHub credential management using chrome.storage.local methods

--- popup.js
+++ popup.js
@@ -1,5 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('pushBtn').addEventListener('click', function () {
+document.addEventListener('DOMContentLoaded', () => {
+
+  const keysToGet = ['githubUsername', 'githubReponame', 'accessToken']
+
+  chrome.storage.local.get(keysToGet, (result) => {
+    if (result.githubUsername) {
+      document.getElementById('git-username').value = result.githubUsername;
+    }
+    if (result.githubReponame) {
+      document.getElementById('git-repo').value = result.githubReponame;
+    }
+    if (result.accessToken) {
+      document.getElementById('git-token').value = result.accessToken;
+    }
+  })
+
+  document.getElementById('pushBtn').addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
@@ -7,10 +22,21 @@ document.addEventListener('DOMContentLoaded', function () {
       })
     })
   })
+
+  document.getElementById('save-btn').addEventListener('click', () => {
+    const githubUsername = document.getElementById('git-username').value
+    const githubReponame = document.getElementById('git-repo').value
+    const accessToken = document.getElementById('git-token').value
+    chrome.storage.local.set({
+      githubUsername, githubReponame, accessToken
+    }, () => {
+      console.log('GitHub credentials saved. ')
+    })
+  })
 })
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+  (request, sender, sendResponse) => {
     if (request.title) {
       console.log("Title:", request.title)
       console.log("URL:", request.url)
@@ -19,3 +45,4 @@ chrome.runtime.onMessage.addListener(
     }
   }
 )
+
